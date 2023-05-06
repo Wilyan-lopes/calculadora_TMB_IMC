7eda193490f19f2f0ad123abff00715e0bcd2f65
Wilyan Lopes <lopes-ph@hotmail.com>
2023-05-06 12:10:40 -0300

    único commit

---
+++ calculadora/imc/CalculadoraImc.java
@@ -0,0 +1,18 @@
+
+package calculadora.imc;
+
+import calculadora.imc.View.TelaPrincipal;
+
+
+public class CalculadoraImc {
+
+  
+    public static void main(String[] args) {
+        
+        TelaPrincipal tela = new TelaPrincipal();
+        
+        tela.setVisible(true);
+       
+    }
+    
+}
